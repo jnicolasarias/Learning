b7903ef92a23f4d4354b69b5b90f15c3e07dba81
Jason Nicolas Arias <60324149+JsNcAr@users.noreply.github.com>
2023-02-24 19:18:05 +0000

    Deleted old files

--- Herramientas_Computacionales_Uniandes/T03/punto1.py
+++
@@ -1,7 +0,0 @@
-
-def funcion ( ):
-	lista = []
-	for var in range (1, 1000 +1):
-		lista.append(var)
-	return lista
-
--- Herramientas_Computacionales_Uniandes/T03/punto4.py
+++
@@ -1,6 +0,0 @@
-import punto1
-import punto2
-import punto3
-
-punto3.funcion3(punto1.funcion())
-
