2cd9d433eb66a48354846c01ff98c9eef3302bf3
Jason Nicolas Arias <60324149+JsNcAr@users.noreply.github.com>
2023-02-24 19:17:53 +0000

    Corrected counter

--- Herramientas_Computacionales_Uniandes/T03_P2/analysis_raven.py
+++ Herramientas_Computacionales_Uniandes/T03_P2/analysis_raven.py
@@ -1,35 +1,28 @@
 # 1. Crear lista con los parrafos del texto
-archivo = open("the_raven_poe.txt")
+archivo = open("the_raven_poe.txt", encoding="utf-8")
 lineas = archivo.readlines()
 texto = ''.join(lineas)
-x=texto.split("\n")
+x = texto.split("\n")
 x.pop(0)
 x.pop(2)
 x.pop(3)
 
 listafinal = []
 for var in x:
-	if var != "":
-		listafinal.append(var)
-print(listafinal)
+    if var != "":
+        listafinal.append(var)
 
-#2 lista I's
+#2 numero I's
 totales = 0
 busqueda = ''.join(listafinal)
-z=busqueda.split()
-w=z.count("I")
-listai = ["I"]*w
+w = busqueda.count("I")
 
-#3 juntar String
-	#se creo el string indicado con nombre busqueda en el punto anterior
-print(busqueda)
+print(f'El texto tiene {w} I\'s')
 
-#4 contar 
+#4 Comparar con i's
 
-total  = z.count("i") + w
-porcentaje = w*100/total
-print(porcentaje)
-
-		
-
-
+total = busqueda.count("i") + w
+porcentaje = w * 100 / total
+print(
+    f'El texto tiene {total} i\'s y {w} I\'s, el porcentaje de I\'s es {porcentaje}%'
+)
